3461236bf88593174efd92150b53aa748d12e05d
Favour Otiger <favourotiger@gmail.com>
2020-03-11 14:34:03 +0100

    PART1 COMPLETE

--- FULLSTACKOPENPART1-a/src/index.js
+++ FULLSTACKOPENPART1-a/src/index.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return(
     <div>
       <p> <Header course={course} /> </p>
-      <p> <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} /> </p>
-      <p> <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/> </p>
+      <p> <Content part1={part1} part2={part2} part3={part3} exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} /> </p>
+      <p> <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/> </p>
     </div>
   )
 }
@@ -29,9 +34,9 @@ const Header = (props) => {
 const Content = (props) => {
   return(
     <div>
-      <p> {props.part1} has {props.exercises1} </p>
-      <p> {props.part2} has {props.exercises2} </p>
-      <p> {props.part3} has {props.exercises3} </p>
+      <p> {props.part1.name} has {props.exercises1} </p>
+      <p> {props.part2.name} has {props.exercises2} </p>
+      <p> {props.part3.name} has {props.exercises3} </p>
 
     </div>
   )
